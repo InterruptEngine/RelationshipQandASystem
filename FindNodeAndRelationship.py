6411559343e008830920b5e8c7c5efe1786de9d4
陈鑫旺 <3280315951@qq.com>
2024-06-03 21:19:18 +0800

    2024年6月3日代码提交

--- FindNodeAndRelationship.py
+++ FindNodeAndRelationship.py
@@ -20,6 +20,10 @@
  '合作院校', '继女', '叔外祖父', '妾', '专职院士数', '主持', '法人', '妯娌', '学校类别', '义弟', '哥哥', '大舅子',
  '义父', '伯伯', '父亲', '外祖母', '第一任丈夫', '外孙子', '玄孙', '姑姑', '师姐', '大舅哥', '队友', '外甥女', '小姨子',  --》陈满
  '堂妹'}
+
+当前的结点{          # 将自己新增加的结点写入
+‘人物','文学作品','文学作品平台'
+}
  """
 if __name__ == '__main__':
     # relationship = set()  # 所有的关系集合
@@ -40,6 +44,7 @@ if __name__ == '__main__':
     infor_dict = dict()
     for myInfor in infor:  # 属性可能相同，但属于不同结点
         if myInfor[1] == '堂姐':
+            # 李慎   堂姐   文安县主
             if '人物' not in infor_dict:
                 infor_dict['人物'] = set()
                 infor_dict['人物'].add(myInfor[0])
@@ -47,8 +52,8 @@ if __name__ == '__main__':
             else:
                 infor_dict['人物'].add(myInfor[0])
                 infor_dict['人物'].add(myInfor[2])
-            # 李慎   堂姐   文安县主
         elif myInfor[1] == '连载平台':  # 此为关系
+            # 花开满园   连载平台   起点中文网
             if '文学作品' not in infor_dict:
                 infor_dict['文学作品'] = set()
                 infor_dict['文学作品'].add(myInfor[0])  # '文学作品  节点'  '添加的内容  属性'
@@ -59,10 +64,131 @@ if __name__ == '__main__':
                 infor_dict['文学作品平台'].add(myInfor[2])
             else:
                 infor_dict['文学作品平台'].add(myInfor[2])
-            # 花开满园   连载平台   起点中文网
-        # elif myInfor[1]=='侄孙媳妇':
-        # elif myInfor[1]=='合作人':
-        # elif myInfor[1]=='文学作品':
-
+        elif myInfor[1] == '侄孙媳妇':
+            # 何应钦 侄孙媳妇 温碧霞
+            if '人物' not in infor_dict:
+                infor_dict['人物'] = set()
+                infor_dict['人物'].add(myInfor[0])
+            else:
+                infor_dict['人物'].add(myInfor[0])
+            if '人物' not in infor_dict:
+                infor_dict['人物'] = set()
+                infor_dict['人物'].add(myInfor[2])
+            else:
+                infor_dict['人物'].add(myInfor[2])
+        elif myInfor[1] == '合作人':
+            # 洪金宝   合作人   袁和平
+            if '人物' not in infor_dict:
+                infor_dict['人物'] = set()
+                infor_dict['人物'].add(myInfor[0])
+            else:
+                infor_dict['人物'].add(myInfor[0])
+            if '人物' not in infor_dict:
+                infor_dict['人物'] = set()
+                infor_dict['人物'].add(myInfor[2])
+            else:
+                infor_dict['人物'].add(myInfor[2])
+        elif myInfor[1] == '文学作品':
+            # 蒋梦麟   文学作品   现代世界中的中国
+            if '人物' not in infor_dict:
+                infor_dict['人物'] = set()
+                infor_dict['人物'].add(myInfor[0])
+            else:
+                infor_dict['人物'].add(myInfor[0])
+            if '文学作品' not in infor_dict:
+                infor_dict['文学作品'] = set()
+                infor_dict['文学作品'].add(myInfor[2])
+            else:
+                infor_dict['文学作品'].add(myInfor[2])
+        elif myInfor[1] == '弟媳':
+            # 洪深   弟媳   钱似莺
+            if '人物' not in infor_dict:
+                infor_dict['人物'] = set()
+                infor_dict['人物'].add(myInfor[0])
+            else:
+                infor_dict['人物'].add(myInfor[0])
+            if '人物' not in infor_dict:
+                infor_dict['人物'] = set()
+                infor_dict['人物'].add(myInfor[2])
+            else:
+                infor_dict['人物'].add(myInfor[2])
+        elif myInfor[1] == '导师':
+            # 王巧   导师   陈爽
+            if '人物' not in infor_dict:
+                infor_dict['人物'] = set()
+                infor_dict['人物'].add(myInfor[0])
+            else:
+                infor_dict['人物'].add(myInfor[0])
+            if '人物' not in infor_dict:
+                infor_dict['人物'] = set()
+                infor_dict['人物'].add(myInfor[2])
+            else:
+                infor_dict['人物'].add(myInfor[2])
+        # elif myInfor[1] == '堂小舅子':
+        # elif myInfor[1] == '摄影作品':
+        # elif myInfor[1] == '大爷爷':
+        # elif myInfor[1] == '师爷':
+        # elif myInfor[1] == '作者':
+        # elif myInfor[1] == '继母':
+        # elif myInfor[1] == '前夫':
+        # elif myInfor[1] == '姑妈':
+        # elif myInfor[1] == '恋人':
+        # elif myInfor[1] == '其他关系':
+        # elif myInfor[1] == '第三任妻子':
+        # elif myInfor[1] == '师妹':
+        # elif myInfor[1] == '第一任妻子':
+        # elif myInfor[1] == '养父':
+        # elif myInfor[1] == '对手':
+        # elif myInfor[1] == '养母':
+        # elif myInfor[1] == '发行专辑':
+        # elif myInfor[1] == '设立单位':
+        # elif myInfor[1] == '女儿':
+        # elif myInfor[1] == '原配':
+        # elif myInfor[1] == '挚爱':
+        # elif myInfor[1] == '主要演员':
+        # elif myInfor[1] == '义妹':
+        # elif myInfor[1] == '曾孙子':
+        # elif myInfor[1] == '成员':
+        # elif myInfor[1] == '侄孙子':
+        # elif myInfor[1] == '养女':
+        # elif myInfor[1] == '公公':
+        # elif myInfor[1] == '历任领导':
+        # elif myInfor[1] == '姨父':
+        # elif myInfor[1] == '堂侄':
+        # elif myInfor[1] == '丈夫':
+        # elif myInfor[1] == '第二任妻子':
+        # elif myInfor[1] == '办学团体':
+        # elif myInfor[1] == '岳父':
+        # elif myInfor[1] == '外祖父':
+        # elif myInfor[1] == '伯父':
+        # elif myInfor[1] == '学弟':
+        # elif myInfor[1] == '师父':
+        # elif myInfor[1] == '伴侣':
+        # elif myInfor[1] == '表哥':
+        # elif myInfor[1] == '学长':
+        # elif myInfor[1] == '妹夫':
+        # elif myInfor[1] == '姐姐':
+        # elif myInfor[1] == '师祖':
+        # elif myInfor[1] == '嫡母':
+        # elif myInfor[1] == '岳母':
+        # elif myInfor[1] == '男友':
+        # elif myInfor[1] == '曾外孙子':
+        # elif myInfor[1] == '舅母':
+        # elif myInfor[1] == '前女友':
+        # elif myInfor[1] == '堂兄':
+        # elif myInfor[1] == '老师':
+        # elif myInfor[1] == '爱人':
+        # elif myInfor[1] == '小叔子':
+        # elif myInfor[1] == '儿子':
+        # elif myInfor[1] == '女朋友':
+        # elif myInfor[1] == '奶奶':
+        # elif myInfor[1] == '综艺节目':
+        # elif myInfor[1] == '恩师':
+        # elif myInfor[1] == '外孙':
+        # elif myInfor[1] == '创始人':
+        # elif myInfor[1] == '员工':
+        # elif myInfor[1] == '执导':
+        # elif myInfor[1] == '侄子':
+        # elif myInfor[1] == '弟子':
     print(infor_dict)  # 上传各个结点以及他们的属性
     #  上传所有的关系
