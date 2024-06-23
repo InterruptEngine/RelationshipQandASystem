c568a99bd9fe1100ea1dc1f8c56120ea488d7b55
陈鑫旺 <3280315951@qq.com>
2024-06-23 14:03:34 +0800

    陈鑫旺__6-23上午编辑框架

--- Analyse_Question.py
+++ Analyse_Question.py
@@ -1,4 +1,5 @@
 import ahocorasick
+import re
 
 
 class AnalyseQuestion:
@@ -44,8 +45,14 @@ class AnalyseQuestion:
         )
         self.all_words_tree = self.build_actree(self.all_words_set)  # 调用actree函数  # 方便快速查找
         self.all_words_dict = self.build_wdtype_dict()  # 各个结点
+        # 问题中的关键词
+        self.possible_isssues()  # 可能提到的问题
+        print("----------加载成功----------")
 
     def build_actree(self, wordlist):
+        """
+        构建trie树，用来查找关键词,方便查找
+        """
         actree = ahocorasick.Automaton()  # 初始化trie树,ahocorasick库 ac自动化 自动过滤违禁数据
         for index, word in enumerate(wordlist):
             actree.add_word(word, (index, word))  # 向trie树中添加词汇，词汇对应的索引
@@ -53,6 +60,9 @@ class AnalyseQuestion:
         return actree  # 方便快速查找
 
     def build_wdtype_dict(self):
+        """
+        各个结点所拥有的标签
+        """
         wd_dict = dict()
         for wd in self.all_words_set:
             wd_dict[wd] = []
@@ -88,3 +98,73 @@ class AnalyseQuestion:
                 wd_dict[wd].append("musicWorks")
         return wd_dict  # 返回各个结点所对应的标签
 
+    def possible_isssues(self):
+        """
+        加载可能遇到的问题
+        """
+        self.question_simpleIssue_list = [r".*的.*是谁", r".*的.*有哪些"]  # 简单问题的关系
+        # egg:浅井初的妹妹是谁？     朱清的伯父有哪些？
+        self.question_countBook_list = ["多少本书", "写过多少本书"]  # 依次找问题写下去
+        # egg:许嘉璐一共写过多少本书？
+
+
+
+
+
+
+
+    def analyze_question(self, question):  # 分析问题
+        """
+        分析问题
+        :param question: 问题
+        :return: {'arg':{结点：标签},'question_types':[标签(问题)所对应的类型]}
+        """
+        data = dict()
+        nodes_dict = self.check_node(question)  # 检查问题中的结点
+        data['arg'] = nodes_dict  # 'arg‘:{结点:标签}
+        types = []  # 问题的类型
+        for type in nodes_dict.values():
+            types += type
+        question_types = []
+        if self.check_wods(self.question_simpleIssue_list, question) and "character" in types:
+            question_types.append("character_to_simpleIssue")
+        if self.check_wods(self.question_countBook_list, question) and "character" in types:
+            question_types.append("character_to_countBook")
+
+
+
+
+
+
+        data['question_types'] = question_types  # 其中遇到的问题类型   # 'question_types’:[问题类型]
+        return data  # {'arg':{结点：标签},'question_types':[标签(问题)所对应的类型]}
+
+    def check_node(self, question):
+        """
+        提取出结点，同时返回每个结点所对应的标签
+        :param question: 问题
+        :return: 返回每个结点所对应的标签
+        """
+        regin_wds = []
+        for i in self.all_words_tree.iter(question):  # ahocorasick库 匹配问题  iter返回一个元组，i的形式如(3, (23192, '小明'))
+            wd = i[1][1]
+            regin_wds.append(wd)
+        stop_wds = []
+        for wd1 in regin_wds:
+            for wd2 in regin_wds:
+                if wd1 in wd2 and wd1 != wd2:
+                    stop_wds.append(wd1)  # stop_wds为其中重复的短的词
+        final_wds = [i for i in regin_wds if i not in stop_wds]
+        final_dict = {i: self.all_words_dict.get(i) for i in final_wds}  # 每个结点所对应的标签
+        return final_dict  # 返回每个结点所对应的标签
+
+    def check_wods(self, question_list, question):
+        """
+        :param question_list: 某种问题类型的关键词
+        :param question: 问题
+        :return: 是否符合正则表达式
+        """
+        for q in question_list:
+            if re.search(q, question):
+                return True
+        return False
--- analyseQuestion.py
+++ analyseQuestion.py
@@ -30,7 +30,7 @@ def kmeans_clustering():
     question_embeddings = model.encode(sample_data)
 
     # 定义要聚类的簇数
-    num_clusters = 350
+    num_clusters = 24
 
     # 使用 K 均值聚类算法进行聚类
     kmeans = KMeans(n_clusters=num_clusters)
@@ -77,30 +77,30 @@ def cosine_similarity_text(str1, str2):
 
 
 if __name__ == '__main__':
-    # kmeans_clustering()
-    vital_question_list = list()
-    with open("vital_question.txt", "r", encoding="utf-8") as fp:
-        for i in fp.readlines():
-            vital_question_list.append(i.strip("\n"))
-    with open("data//test_qa.json", "r", encoding="utf-8") as fp:
-        data = json.loads(fp.read())
-    test_qa_list = [data[i]["question"] for i in data.keys()]
-    # print(vital_question_list)
-    # print(test_qa_list)
-    total_max = 0  # 计算总和
-    extra_question_list = list()
-    for i in range(len(test_qa_list)):  # 计算相似度
-        flag = 0
-        max = 0
-        for j in range(len(vital_question_list)):
-            result = cosine_similarity_text(test_qa_list[i], vital_question_list[j])
-            if max < result:
-                flag = j
-                max = result
-        if max < 0.15:  # 相似度小于0.15的就加入到其中
-            extra_question_list.append(test_qa_list[i])
-        print(test_qa_list[i], vital_question_list[flag], max)
-        total_max += max
-    print("相似度之和为{0}".format(total_max / len(test_qa_list)))
-    with open("real_question.txt", "w", encoding="utf-8") as fp:
-        fp.write("\n".join(vital_question_list + extra_question_list))
+    kmeans_clustering()
+    # vital_question_list = list()
+    # with open("vital_question.txt", "r", encoding="utf-8") as fp:
+    #     for i in fp.readlines():
+    #         vital_question_list.append(i.strip("\n"))
+    # with open("data//test_qa.json", "r", encoding="utf-8") as fp:
+    #     data = json.loads(fp.read())
+    # test_qa_list = [data[i]["question"] for i in data.keys()]
+    # # print(vital_question_list)
+    # # print(test_qa_list)
+    # total_max = 0  # 计算总和
+    # extra_question_list = list()
+    # for i in range(len(test_qa_list)):  # 计算相似度
+    #     flag = 0
+    #     max = 0
+    #     for j in range(len(vital_question_list)):
+    #         result = cosine_similarity_text(test_qa_list[i], vital_question_list[j])
+    #         if max < result:
+    #             flag = j
+    #             max = result
+    #     if max < 0.15:  # 相似度小于0.15的就加入到其中
+    #         extra_question_list.append(test_qa_list[i])
+    #     print(test_qa_list[i], vital_question_list[flag], max)
+    #     total_max += max
+    # print("相似度之和为{0}".format(total_max / len(test_qa_list)))
+    # with open("real_question.txt", "w", encoding="utf-8") as fp:
+    #     fp.write("\n".join(vital_question_list + extra_question_list))
